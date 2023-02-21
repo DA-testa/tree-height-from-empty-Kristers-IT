da9b0640658dc2b4e4f9f552ab4fd52814288a50
Kristers Vilks <112978132+Kristers-IT@users.noreply.github.com>
2023-02-21 13:30:12 +0000

    koks

--- tree_height.py
+++ tree_height.py
@@ -2,6 +2,8 @@
 
 import sys
 import threading
+import numpy as np
+import re
 
 
 def compute_height(n, parents):
@@ -13,6 +15,31 @@ def compute_height(n, parents):
 
 def main():
     # implement input form keyboard and from files
+    text = input()
+    if 'I' in text[:1]:
+        elementu_skaits = input("Ievadi elementu skaitu: ")
+    elif 'F' in text[:1]:
+        if 'a' in text:
+            print("Nosaukums satur burtu a")
+            exit()
+        else:
+            text = input("Faila nosaukums: ")
+            with open(text, "r") as f:
+                text = f.read()
+                elementu_skaits = input("Ievadi elementu skaitu: ")
+    else:
+        print("Nepareizs ievades formāts")
+        
+    vertibas = []
+
+    for i in range(elementu_skaits):
+        skaitli = int(input("Ievadi šos skaitļus: "))
+        vertibas.append(skaitli)
+
+    str_vertibas = map(str, vertibas)
+    str_vertibas = ' '.join(str_vertibas)
+
+    print(str_vertibas)
     
     # let user input file name to use, don't allow file names with letter a
     # account for github input inprecision
