feadf58d2db6b1ef1b5fc21902ad2546fff86a73
Kristers Vilks <112978132+Kristers-IT@users.noreply.github.com>
2023-02-24 15:16:18 +0000

    versija_2.0

--- tree_height.py
+++ tree_height.py
@@ -1,57 +1,50 @@
 # python3
-
 import sys
 import threading
 import numpy as np
-import re
+import urllib.request
+import io
 
 
-def compute_height(n, parents):
+def compute_height(elementu_skaits, vertibas):
     # Write this function
     max_height = 0
-    # Your code here
+  
+    for virsotne in range(elementu_skaits):
+      height = 0
+      i = virsotne
+      while i != -1:
+        height = height + 1
+        i = vertibas[i] 
+      max_height = max(max_height,height)
+    
     return max_height
 
 
 def main():
-    # implement input form keyboard and from files
+
     text = input()
-    if 'I' in text[:1]:
-        elementu_skaits = input("Ievadi elementu skaitu: ")
-    elif 'F' in text[:1]:
-        if 'a' in text:
-            print("Nosaukums satur burtu a")
-            exit()
-        else:
-            text = input("Faila nosaukums: ")
-            with open(text, "r") as f:
-                text = f.read()
-                elementu_skaits = input("Ievadi elementu skaitu: ")
-    else:
-        print("Nepareizs ievades formāts")
-        
-    vertibas = []
+    if text.startswith('I'):
+        elementu_skaits = int(input("Ievadi elementu skaitu: "))
+        skaitli = input("Ievadiet vērtības: ")
+    elif text.startswith('F'):
+        nosaukums = input("Faila nosaukums: ") 
+        if 'a' or 'A' in nosaukums:
+           return
+        with urllib.request.urlopen(nosaukums) as response:
+            dati = response.read().decode()
+            f = io.StringIO(dati)
+            for rinda in f.readlines():
+                elementu_skaits = rinda[0]
+                skaitli = rinda[1]
+   
+    vertibas = np.zeros(elementu_skaits, dtype=int)
+    skaitli = [int(skaitli) for skaitli in skaitli.split()]
+    vertibas = np.array(skaitli)
+    max_height = compute_height(elementu_skaits, vertibas)
 
-    for i in range(elementu_skaits):
-        skaitli = int(input("Ievadi šos skaitļus: "))
-        vertibas.append(skaitli)
-
-    str_vertibas = map(str, vertibas)
-    str_vertibas = ' '.join(str_vertibas)
-
-    print(str_vertibas)
+    print(max_height)
     
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
-
-
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
+sys.setrecursionlimit(10**7)  
+threading.stack_size(2**27)   
 threading.Thread(target=main).start()
