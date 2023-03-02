673752c267057745538b4e64d74cd7650388ada7
Kristers Vilks <112978132+Kristers-IT@users.noreply.github.com>
2023-03-02 22:47:19 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -6,24 +6,23 @@ import numpy as np
 def compute_height(elementu_skaits, vertibas):
 
     max_height = 0
-    saraksts [0] * elementu_skaits
+    saraksts = [0] * elementu_skaits
 
     for i in range(elementu_skaits):
       height = get_height(i, vertibas, saraksts)
-
       if height > max_height:
         max_height = height
         
     return max_height
 
 def get_height(i, vertibas, saraksts):
-    
+
     if saraksts[i] != 0:
         return saraksts[i]
-    
+
     if vertibas[i] == -1:
         saraksts[i] = 1
- 
+
     else:
         saraksts[i] = get_height(vertibas[i], vertibas, saraksts) + 1
     return saraksts[i]
@@ -33,7 +32,7 @@ def main():
     if text.startswith('I'):
         elementu_skaits = int(input("Ievadi elementu skaitu: "))
         vertibas = np.asarray(list(map(int,input("Ievadiet vērtības: ").split())))
-
+        
     elif text.startswith('F'):
         nosaukums = input("Ievadi faila nosaukumu: ") 
         if 'a' in nosaukums:
@@ -41,11 +40,11 @@ def main():
         fails = open("./test/" + nosaukums, "r")
         elementu_skaits = int(fails.readline())
         vertibas = np.asarray(list(map(int,fails.readline().split())))
-    
+
     max_height = compute_height(elementu_skaits, vertibas)
- 
+
     print(max_height)
-    
+
 sys.setrecursionlimit(10**7)  
 threading.stack_size(2**27)   
 threading.Thread(target=main).start()
